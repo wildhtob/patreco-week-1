d50b32935f1d40415b5a5d329a52d701197c9e72
Tobias Wildhaber <israfael@me.com>
2021-04-23 12:23:26 +0200

    Added spatial data sf-object

--- scripts/u-week-1.R
+++ scripts/u-week-1.R
@@ -3,6 +3,7 @@
 library(readr)        # to import tabular data (e.g. csv)
 library(dplyr)        # to manipulate (tabular) data
 library(ggplot2)      # to visualize data
+library(sf)           # handling spatial data
 
 # Task 1: Import data ####
 
@@ -13,3 +14,21 @@ str(wildschwein)
 
 ggplot(wildschwein, aes(Long, Lat, colour = TierID)) +
   geom_point()
+
+# Handling spatial data ####
+
+wildschwein_sf <- st_as_sf(wildschwein, coords = c("Long", "Lat"), crs = 4326)
+
+wildschwein_sf
+
+# subset rows
+wildschwein_sf[1:10,]
+wildschwein_sf[wildschwein_sf$TierName == "Sabi",]
+
+# subset colums
+wildschwein_sf[,2:3]
+
+# replace initial data frame
+
+wildschwein <- st_as_sf(wildschwein, coords = c("Long", "Lat"), crs = 4326)
+rm(wildschwein_sf)
