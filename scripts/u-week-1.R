ddaa8ac37b62b1d15bfde761fe8e4481b31969a2
Tobias Wildhaber <israfael@me.com>
2021-04-23 11:46:28 +0200

    completed Task 2

--- scripts/u-week-1.R
+++ scripts/u-week-1.R
@@ -4,7 +4,12 @@ library(readr)        # to import tabular data (e.g. csv)
 library(dplyr)        # to manipulate (tabular) data
 library(ggplot2)      # to visualize data
 
-# Import data ####
+# Task 1: Import data ####
 
 wildschwein = read_delim("./data/wildschwein_BE.csv", delim = ",")
 str(wildschwein)
+
+# Task 2: Explore Data ####
+
+ggplot(wildschwein, aes(Long, Lat, colour = TierID)) +
+  geom_point()
